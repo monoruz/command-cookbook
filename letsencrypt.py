7ab8878aba71bf073c44ebf805432250073ffc5b
mm74noroozi <41954275+mm74noroozi@users.noreply.github.com>
2022-10-23 15:17:45 +0330

    Create letsencrypt.py

---
+++ letsencrypt.py
@@ -0,0 +1,45 @@
+
+#!/usr/bin/python3
+
+import os
+import time
+import subprocess
+
+command = [
+    "certbot",
+    "-n", "--agree-tos", # non-interactive
+    "-d", os.environ["HOSTNAMES"],
+    "-m", "{}@{}".format(os.environ["POSTMASTER"], os.environ["DOMAIN"]),
+    "certonly", "--standalone",
+    "--cert-name", "mailu",
+    "--preferred-challenges", "http", "--http-01-port", "8008",
+    "--keep-until-expiring",
+    "--allow-subset-of-names",
+    "--renew-with-new-domains",
+    "--config-dir", "/certs/letsencrypt",
+    "--post-hook", "/config.py"
+]
+command2 = [
+    "certbot",
+    "-n", "--agree-tos", # non-interactive
+    "-d", os.environ["HOSTNAMES"],
+    "-m", "{}@{}".format(os.environ["POSTMASTER"], os.environ["DOMAIN"]),
+    "certonly", "--standalone",
+    "--cert-name", "mailu-ecdsa",
+    "--preferred-challenges", "http", "--http-01-port", "8008",
+    "--keep-until-expiring",
+    "--allow-subset-of-names",
+    "--key-type", "ecdsa",
+    "--renew-with-new-domains",
+    "--config-dir", "/certs/letsencrypt",
+    "--post-hook", "/config.py"
+]
+
+# Wait for nginx to start
+time.sleep(5)
+
+# Run certbot every day
+while True:
+    subprocess.call(command)
+    subprocess.call(command2)
+    time.sleep(86400)
